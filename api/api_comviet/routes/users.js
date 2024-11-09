a3aaf26890471482b9c71c9cb63fe0e08d65f876
Admin <nguyenkhachieu1512@gmail.com>
2024-11-09 22:52:19 +0700

    Remove api directory

--- api/api_comviet/routes/users.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const uc = require("../controllers/user.controller")
-
-router.post('/net',uc.reg)
-router.post('/login',uc.login)
-module.exports = router;
